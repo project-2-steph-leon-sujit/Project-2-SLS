02a7d5baf18de80b4af3d915749e37c702a1734b
Leon Lin <leonlin310@gmail.com>
2019-06-24 21:29:22 -0500

    new working files on new branch

---
+++ schema.sql
@@ -0,0 +1,5 @@
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS budget;
+-- Creates the "blogger" database --
+CREATE DATABASE budget;
+
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 
 require('dotenv').config();
 
-var express = require('express');
+var express = require('express'); 
 var app = express();
-var port = process.env.PORT || 9000;
+var PORT = process.env.PORT || 9000;
 var config = require('./config');
 
+
 app.use(express.static(__dirname + '/views')); // you should change this to be wherever your html files are
+
+// Requiring our models for syncing
+var db = require("./models");
+
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.listen(port);
+require("./routes/budget-api-routes.js")(app);
 
-//@TODO Delete below after you verify the the app is working
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
+  
+// app.listen(port);
+
+// //@TODO Delete below after you verify the the app is working
 app.route('/').get(function(request, response) {
     response.json(config);
 });
+
+// // console.log(db)
