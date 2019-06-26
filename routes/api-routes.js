76d50f8b64a46f3f0681c57e66d47ebde3677618
sujit shah <a.sujitshah34@gmail.com>
2019-06-26 17:14:21 -0500

    api

---
+++ routes/api-routes.js
@@ -0,0 +1,34 @@
+// Pull in required dependencies
+var path = require('path');
+
+// Import the list of budget entries
+var budget = require('../models/budget.js');
+
+// Export API routes
+module.exports = function (app) {
+    // console.log('___ENTER apiRoutes.js___');
+
+    // Total list of budget entries
+    app.get('/api/models/budget', function (req, res) {
+        res.json(budget);
+
+    });
+
+    // Add new budget entry
+    app.post('/api/data/friends', function (req, res) {
+
+
+
+
+    }
+
+
+
+
+
+    // Add new user
+		budget.push(userInput);
+
+    // Send appropriate response
+    res.json({ status: 'OK', matchName: matchName, matchImage: matchImage });
+});
