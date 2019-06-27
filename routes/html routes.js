c8e292c43bf273e53c3314dcea7e20eb118dbe58
Stephanie Sapp <stephs1531@gmail.com>
2019-06-26 21:55:36 -0500

    changing back to html

--- routes/html routes.js
+++ routes/html routes.js
@@ -1,7 +1,12 @@
-app.get('/', function(req, res, next) {
-    res.render('home', {layout: 'main', template: 'home-template'});
-  });
+
+module.exports = function (app) {
+
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/blog.html"));
+      });
 
 app.get('/budget', function(req, res, next) {
 res.render('budget', {layout: 'main', template: 'budget-template'});
 });
+
+};
