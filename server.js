1fdbbe52b060415471a67b25662b81823fc7ac10
Rich Hosek <richhosek@gmail.com>
2019-06-26 20:28:55 -0500

    updated server.jst from master branch

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
-
 require('dotenv').config();
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 9000;
+var PORT = process.env.PORT || 9000;
 var config = require('./config');
 
+
 app.use(express.static(__dirname + '/views')); // you should change this to be wherever your html files are
-app.use(express.urlencoded({extended: true}));
+
+// Requiring our models for syncing
+var db = require("./models");
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-app.listen(port);
+require("./routes/budget-api-routes.js")(app);
 
-//@TODO Delete below after you verify the the app is working
-app.route('/').get(function(request, response) {
+db.sequelize.sync({
+    force: true
+}).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
+    });
+});
+
+// app.listen(port);
+
+// //@TODO Delete below after you verify the the app is working
+app.route('/').get(function (request, response) {
     response.json(config);
 });
 
+// // console.log(db)
